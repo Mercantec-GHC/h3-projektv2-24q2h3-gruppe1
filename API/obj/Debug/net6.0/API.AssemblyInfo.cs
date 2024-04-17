915c7a52053d2314916c731fac4b04d60ff736b1
Turbobent <kasp728h@edu.mercantec.dk>
2024-04-17 08:40:20 +0200

    installed entityFramework

--- API/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+608b65f402a3f62197171f9e94eef59c8c71ae3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1913ac8e414cd619f42b5fe75c6cc15d97542da")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

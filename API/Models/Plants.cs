bb65ccdabd4b5d75425ab81b0c02925cbc49c07e
Turbobent <kasp728h@edu.mercantec.dk>
2024-04-25 10:11:35 +0200

    added plant model

---
+++ API/Models/Plants.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models
+{
+    public class Plants : Common
+    {
+        public string PlantName { get; set; }
+        public int MinWaterLevel { get; set; }
+        public int MaxWaterLevel { get; set; }
+    }
+}

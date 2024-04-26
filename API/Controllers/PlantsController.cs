46f03e93ead552ac15d3b54247d1233c9e9cbd5e
Turbobent <kasp728h@edu.mercantec.dk>
2024-04-26 09:07:08 +0200

    changes to plant controller

--- API/Controllers/PlantsController.cs
+++ API/Controllers/PlantsController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using API.Data;
 using API.Models;
+
 
 namespace API.Controllers
 {

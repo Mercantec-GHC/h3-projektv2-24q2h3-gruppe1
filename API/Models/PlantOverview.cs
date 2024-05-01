977da1f96ce21468ed466c72db107737b6f4d89c
Turbobent <kasp728h@edu.mercantec.dk>
2024-05-01 10:16:19 +0200

    fixed error 500 post

--- API/Models/PlantOverview.cs
+++ API/Models/PlantOverview.cs
@@ -4,7 +4,7 @@ namespace API.Models
 {
     public class PlantOverview 
     {
-        public Plant PlantName { get; set; }
+        public int PlantNameId { get; set; }
         public int MoistureLevel { get; set; }
         public int MinWaterLevel { get; set; }
         public int MaxWaterLevel { get; set; }

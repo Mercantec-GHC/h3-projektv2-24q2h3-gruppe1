3b101b8c64bd540577eb8e49fbbc185667ad18ca
TGSXVII <madsduly@gmail.com>
2024-06-03 08:23:09 +0200

    a bunch new shit figure it out

---
+++ API/Controllers/FileController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+[Route("File")]
+public class FileController : Controller
+{
+    public IActionResult GetPdf(string fileName)
+    {
+        // Combine the base directory with the wwwroot/files directory and the file name
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", fileName);
+
+        // Ensure the file exists
+        if (!System.IO.File.Exists(filePath))
+        {
+            return NotFound();
+        }
+
+        var fileBytes = System.IO.File.ReadAllBytes(filePath);
+        return File(fileBytes, "application/pdf");
+    }
+}
